dde66c22d89231b51b5b8d8efd35dc5c07c612c2
Dyamans <dgpats@gmail.com>
2019-01-03 15:27:10 +1000

    re initialized commit 6

--- NameSorterTests/NameSortingTests.cs
+++ NameSorterTests/NameSortingTests.cs
@@ -1,29 +1,32 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections;
 using System.Collections.Generic;
- 
+using NUnit.Framework;
+
 
 namespace NameSorterTests
 {
-    [TestClass]
+    [TestFixture]
     public class NameSortingTests
     {
 
         private NameSorter.ISorter sorter = null;        
         private String unsortedFile = null;
         private string sortedFile = null;
+        private string getPath = null;        
 
-        [TestInitialize]
-        public void TestInit()
+        [SetUp]
+        public void SetUp()
         {
             sorter = new NameSorter.Sorting();
-            unsortedFile = "./unsorted-names-list.txt";
-            sortedFile = "./sorted-names-list.txt";
+            unsortedFile = "/unsorted-names-list.txt";
+            sortedFile = "/sorted-names-list.txt";
+            getPath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
         }
 
-        [TestCleanup]
-        public void TestClean()
+        [TearDown]
+        public void TearDown()
         {
             sorter = null;
             unsortedFile = string.Empty;
@@ -31,7 +34,7 @@ namespace NameSorterTests
         }
 
 
-        [TestMethod]
+        [Test]
         public void Test_ReadData()
         {
 
@@ -47,11 +50,11 @@ namespace NameSorterTests
 
         }        
 
-        [TestMethod]
+        [Test]
         public void Test_ValidateData()
         {
             //Arrange                       
-            List<Object> personData = sorter.ReadData(unsortedFile);
+            List<Object> personData = sorter.ReadData(getPath+unsortedFile);
 
             //Assert
             Assert.AreEqual(11, personData.Count);
@@ -60,13 +63,13 @@ namespace NameSorterTests
             Assert.AreEqual("Frankie Conner Ritter", personData[10]);
         }
 
-        [TestMethod]
+        [Test]
         public void Test_WriteData()
         {
             //Arrange
-            List<Object> personData = sorter.ReadData(unsortedFile);
+            List<Object> personData = sorter.ReadData(getPath+unsortedFile);
             List<object> properData = sorter.ValidateData(personData);
-            sorter.WriteData(sortedFile, properData);
+            sorter.WriteData(getPath+sortedFile, properData);
 
             //Assert
             Assert.AreEqual(11, personData.Count);
@@ -76,17 +79,20 @@ namespace NameSorterTests
         }
 
 
-        [TestMethod] 
+        [Test] 
         public void Test_ShowData()
         {
             //Arrange           
-            List<Object> personData = sorter.ReadData(unsortedFile);
+            List<Object> personData = sorter.ReadData(getPath+unsortedFile);
             List<object> properData = sorter.ValidateData(personData);
-            sorter.WriteData(sortedFile, properData);
+            sorter.WriteData(getPath+sortedFile, properData);
             sorter.ShowData(properData);
 
             //Assert 
+            Assert.AreEqual("Alvarez", ((NameSorter.Person)properData[0]).LastName);
             Assert.AreEqual("Shelby", ((NameSorter.Person)properData[10]).FirstName);
+            
+
         }
     }
 }
